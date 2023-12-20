15f1432a38da6e659084343d363192658e865fa8
Romain Laurence <mw3pumat8@gmail.com>
2023-12-20 03:33:18 +0100

    import du fichier .env dans le serveur

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import express from 'express';
+import express from "express";
 import "reflect-metadata";
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
-
-app.get('/', (req, res) => {
-    res.send('Salut sa marche')
+app.get("/", (req, res) => {
+  res.send("Salut sa marche");
 });
 
 app.listen(port, () => {
-    
-    console.log(`server is listening on ${port}`);
+  console.log(`server is listening on ${port}`);
 });
