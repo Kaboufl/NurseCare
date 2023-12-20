2c3d857a2625f8c10d90c730f1f74c2eb473dc95
Romain Laurence <mw3pumat8@gmail.com>
2023-12-20 04:13:11 +0100

    ajout mécanique de génération de token d'authentification

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,38 @@ import express from "express";
 import "reflect-metadata";
 require("dotenv").config();
 
+const jwt = require("jsonwebtoken");
+
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Salut sa marche");
 });
 
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
+app.get("/JWT", (req, res) => {
+  /**
+   * Cette fonction génère un token en prenant un object en entrée, ainsi qu'une clé secrète
+   * @param jsonData
+   * @param options
+   * @returns
+   */
+  const createTokenFromJson = (jsonData: {}, options = {}) => {
+    try {
+      const secretKey = "test";
+      const token = jwt.sign(jsonData, secretKey, options);
+      return token;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const jsonData = { email: "test@test.com", password: "motdepassefort" };
+  const token = createTokenFromJson(jsonData);
+
+  res.json({ status: true, token: token });
+});
+
+app.listen(process.env.API_PORT, () => {
+  console.log(`server is listening on ${process.env.API_PORT}`);
 });
