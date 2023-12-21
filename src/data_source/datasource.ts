90384e27a2742e1191b11fef62dd5799dc8d16ed
bagkrep <bagbred@outlook.com>
2023-12-21 21:58:44 +0100

    Added HOST and PORT

--- src/data_source/datasource.ts
+++ src/data_source/datasource.ts
@@ -2,8 +2,8 @@ import { DataSource } from "typeorm"
 
 const connexion = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
