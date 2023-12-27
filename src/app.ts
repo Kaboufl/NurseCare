fecda2a3c67ade9352d60c16e2a00279de1c326f
Romain Laurence <mw3pumat8@gmail.com>
2023-12-27 17:14:02 +0100

    tests concluents pour lecture/écriture BDD

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,44 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-
-import express from 'express';
+import express from "express";
 import "reflect-metadata";
 
+import { Personnel } from "./entity/personnel";
 
-import NCDB from './data_source/datasource';
-
+import NCDB from "./data_source/datasource";
 
 const app = express();
 const port = 3000;
 
+app.get("/", async (req, res) => {
+  const personnel = await (await NCDB()).manager.find(Personnel);
 
-app.get('/', (req, res) => {
-    res.send('Salut sa marche')
+  console.log(personnel);
+  res.send(personnel);
+});
+
+app.get("/add", async (req, res) => {
+  const manager = (await NCDB()).manager;
+
+  const newPersonnel = manager.create(Personnel, {
+    nom: "Timber",
+    prenom: "Saw",
+    adresse: "20 rue des potiers",
+    tel: "0123456789",
+    etablissement: 4,
+    role: 7,
+  });
+
+  try {
+    manager.save(newPersonnel);
+  } catch (error) {
+    console.error("Le personnel n'a pas pu être ajouté");
+  }
+
+  res.send("personnel ajouté");
 });
 
 app.listen(port, async () => {
-    
-    console.log(`server is listening on ${port}`);
-
-    console.log(await NCDB())
+  console.log(`server is listening on ${port}`);
 });
--- src/data_source/datasource.ts
+++ src/data_source/datasource.ts
@@ -1,33 +1,33 @@
-import { DataSource } from "typeorm"
+import { DataSource } from "typeorm";
 
-import { Personnel } from "../entity/personnel"
-import { Etablissement } from "../entity/etablissement"
+import { Personnel } from "../entity/personnel";
+import { Etablissement } from "../entity/etablissement";
 
-const NCDB = async () => {
-    const connexion = new DataSource({
-        type: "mysql",
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [Personnel, Etablissement],
-        synchronize: true
-    })
+const NCDB = async (): Promise<DataSource> => {
+  const datasource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [Personnel, Etablissement],
+    synchronize: true,
+  });
 
-    const { dirname } = require('path');
-    const appDir = require.main ? dirname(require.main.filename) : '';
-    console.log("Dossier : ", appDir)
+  const { dirname } = require("path");
+  const appDir = require.main ? dirname(require.main.filename) : "";
+  console.log("Dossier : ", appDir);
 
-    try {
-        await connexion.initialize()
-        console.log("Data Source initialisée!")
+  try {
+    await datasource.initialize();
+    console.log("Data Source initialisée!");
 
-        return connexion
-    } catch (err) {
-        console.error("Erreur pendant l'initialisation du Data Source", err)
-        return false
-    }
-}
+    return datasource;
+  } catch (err) {
+    console.error("Erreur pendant l'initialisation du Data Source", err);
+    throw err;
+  }
+};
 
-export default NCDB
+export default NCDB;
