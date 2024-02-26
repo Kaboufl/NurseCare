f56c7ff6822a8c5a33c529498bb08b80e94305e3
bagkrep <bagbred@outlook.com>
2024-02-26 11:24:39 +0100

    Feature envoi mail facture terminée ?

--- src/controllers/PrestationController.ts
+++ src/controllers/PrestationController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { prisma } from "../app"
 import nodemailer from "nodemailer";
 import { jsPDF } from "jspdf";
-import { readFileSync } from "fs";
+// import { readFileSync } from "fs";
 
 export class PrestationController {
 
@@ -38,14 +38,18 @@ export class PrestationController {
                         personnel:true,
                     }
                 },
-                soin: true,
+                soin:{
+                    include: {
+                        categorie: true,
+                    }
+                }
                 }
             })
         const transporter = nodemailer.createTransport({
             service: 'smtp.office365.com',
             auth: {
-                user:'',
-                pass:''
+                user:'nurseCare4R7@outlook.com',
+                pass:'nrsCr31200!!'
             }
         })
         const pdf = new jsPDF();
@@ -57,18 +61,44 @@ export class PrestationController {
           });
 
         const titleHeight = titleLines.length * 10;
-        const data = this.prestationsFiltrees.map(intervention => [intervention.libelle_prestation, `${intervention.prix_prestation} €`]);
+        const data = prestations.map(prestations => [prestations.soin.libelle, prestations.soin.categorie.libelle, `${prestations.soin.prix} €`]);
+        const totalPrix = prestations.reduce((total, prestations) => total + prestations.soin.prix.toNumber(), 0);
+        data.push(['Total', `${totalPrix} €`]);
     
+        (pdf as any).autoTable({
+            head: [['Soin Appliqué', 'Type de Soin', 'Prix']],
+            body: data,
+            startY: 20 + titleHeight,
+          });
+        
+        const fullDate = `${prestation.date.getDate()}${prestation.date.getMonth() + 1}${prestation.date.getFullYear()}${prestation.date.getHours()}${prestation.date.getMinutes()}`;
+        const fullDateNoTime = `${prestation.date.getDate()}/${prestation.date.getMonth() + 1}/${prestation.date.getFullYear()}`;
+        console.log(fullDateNoTime);
+        const fullLetterDateOptions = {weekday: "long", year: "numeric", month: "long", day: "numeric"};
+        const fullLetterDate = ``;
+
+        const pdfFileName = `fact-${fullDate}-${prestation.id}.pdf`;
+        pdf.save(pdfFileName);
+        const fakePdfBase64 = pdf.output('datauristring');
+        
+        const mailContent = {
+            from: "noreply-nursecare@outlook.com",
+            to: prestation.patient.mail,
+            subject: `Intervention du ${fullDateNoTime}`,
+            text: `Bonjour Madame/Monsieur ${prestation.patient.nom}, voici la facture de votre intervention du ${fullLetterDate} ci-joint. Total: ${totalPrix} €`,
+            attachments: [
+              {
+                filename: pdfFileName,
+                content: fakePdfBase64.split(',')[1],
+                encoding: 'base64',
+              },
+            ],
+          };
+
         //const html = readFileSync("./email.html").toString("utf8").replace(/{{(.*?)}}/g,(match) => {
         //    return prestations[0].toString() || ''});
-    
-        const mailOptions = {
-            from:'noreply-nursecareAS@nursecare.org',
-            to:'',
-            subject:'Facture Intervention du ???',
-        }
-    
-        transporter.sendMail(mailOptions, (err,info) => {
+
+        transporter.sendMail(mailContent, (err,info) => {
             if (err) {
                 console.log(err);
                 res.status(500).send("Erreur lors de l'envoi du mail");
