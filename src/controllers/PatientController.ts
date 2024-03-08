ad720d1605f630381697ffce22df4983197eaa46
Kaboufl <mw3pumat8@gmail.com>
2024-03-08 22:13:23 +0100

    Modification patient ok, CRUD complet

--- src/controllers/PatientController.ts
+++ src/controllers/PatientController.ts
@@ -26,6 +26,35 @@ const PatientController = {
         }
     },
 
+    async updatePatient(req: Request, res: Response) {
+        try {
+            delete req.body.id
+            const updatedPatient = req.body
+            const patientId: number = parseInt(req.params.id)
+            
+            
+            const patient = await prisma.patient.update({
+                where: {
+                    id: patientId
+                },
+                data: updatedPatient
+            })
+            
+            res.status(200).json({
+                statut: "ok",
+                message: "Patient mis à jour",
+                patient: patient
+            })
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({
+                statut: "error",
+                message: "Echec lors de la mise à jour du patient",
+                error: error
+            })
+        }
+    },
+
     async deletePatient(req: Request, res: Response) {
         console.log(req.params)
         try {
@@ -37,8 +66,12 @@ const PatientController = {
             res.status(200).json({
                 message: "Patient supprimé"
             })
-        } catch (error) {
-            console.error(error)
+        } catch (error: any) {
+            if(error.code == "P2003") {
+                return res.status(409).json({
+                    message: "Le patient possède des interventions assignées, impossible de le supprimer"
+                })
+            }
             res.status(500).json({
                 message: "Echec de la suppression du patient",
                 error: error
